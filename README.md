2340d8eaeb7c1601d5b28b6a99e57523e09bdab3
hhs <user20@ip-172-31-22-99.ap-northeast-1.compute.internal>
2023-05-11 02:55:38 +0000

    copy commit

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
-# front-app-repo
-#test
-front-app-repo by hhs
+# Amazon EKS Demo 
+This repo holds **React Frontend sample** for [Amazon EKS Hands on Lab](https://master.d3s71i2n51x60t.amplifyapp.com/ko/)
---
+++ build/precache-manifest.c436dcbc0d5fd74e5c7ef81d9f43de94.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f518866d0804339300a882e6aef4fe2b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4157da175f1e988f6aa5",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "879c1f8800d3fd79cc31",
+    "url": "/static/js/2.011518c1.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.011518c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4157da175f1e988f6aa5",
+    "url": "/static/js/main.36d3715e.chunk.js"
+  },
+  {
+    "revision": "21f27347af35e687715a",
+    "url": "/static/js/runtime-main.d5ab784a.js"
+  }
+]);

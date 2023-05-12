c94bede3f067aea9582a8b83067db696da22a360
hhs <user20@ip-172-31-22-99.ap-northeast-1.compute.internal>
2023-05-12 08:47:43 +0000

    Modified App.js---update2

--- src/App.js
+++ src/App.js
@@ -64,7 +64,7 @@ function App(props) {
             align="center"
             className={classes.title}
           >
-            EKS DEMO Blog by hhs---update1
+            EKS DEMO Blog by hhs---update2
           </Typography>
           {new Date().toLocaleTimeString()}
         </Toolbar>
